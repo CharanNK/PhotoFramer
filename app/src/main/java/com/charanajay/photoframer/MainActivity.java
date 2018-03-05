3ee618931da91bb4c38dfca3a0a460f7ad2b7aed
CharanNK <charkeet@gmail.com>
2018-03-05 21:59:10 +0530

    Initial commit. app starting

---
+++ app/src/main/java/com/charanajay/photoframer/MainActivity.java
@@ -0,0 +1,21 @@
+package com.charanajay.photoframer;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
